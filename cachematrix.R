5557c88c796cdb9aa61606d7e4aae58a130c184d
HasanMusa <72334663+HasanMusa@users.noreply.github.com>
2025-03-29 17:10:26 +0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+hms_cmatrix <- function(hms_im=matrix()) {
+    hms_ci <- NULL
+    hms_setM <- function(hms_nm) {
+        hms_im <<- hms_nm
+        hms_ci <<- NULL
+    }
+    hms_getM <- function() hms_im
+    hms_setI <- function(hms_inv) hms_ci <<- hms_inv
+    hms_getI <- function() hms_ci
+    list(hms_setM=hms_setM, hms_getM=hms_getM, hms_setI=hms_setI, hms_getI=hms_getI)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+hms_csolve <- function(hms_mo, ...) {
+    hms_ci <- hms_mo$hms_getI()
+    if (!is.null(hms_ci)) {
+        message("Returning cached inverse")
+        return(hms_ci)
+    }
+    hms_cm <- hms_mo$hms_getM()
+    hms_cinv <- solve(hms_cm, ...)
+    hms_mo$hms_setI(hms_cinv)
+    hms_cinv
 }
+
+hms_em <- matrix(c(2,1,1,4),2,2)
+hms_sm <- hms_cmatrix(hms_em)
+hms_csolve(hms_sm)
+hms_csolve(hms_sm)

ea379db25a0507a505021870990e521a4741e7e4
srikg20 <126743544+srikg20@users.noreply.github.com>
2025-06-05 18:22:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its median.
+## It includes functions to set and get the matrix, and to set and get the cached median.
+
+
+## This function computes the median of the special "matrix" returned by makeCacheMatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                m <- NULL
+            set <- function(y) {
+                    x <<- y
+                    m <<- NULL
+            }
+            get <- function() x
+            setmedian <- function(median) m <<- median
+            getmedian <- function() m
+            list(set = set, get = get,
+                 setmedian = setmedian,
+                 getmedian = getmedian)
+    
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## If the median has already been calculated (and the matrix has not changed),
+## then it retrieves the median from the cache.
+
+
+
+        cacheMedian <- function(x, ...) {
+    m <- x$getmedian()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- median(data, ...)
+    x$setmedian(m)
+    m
 }
+
+
